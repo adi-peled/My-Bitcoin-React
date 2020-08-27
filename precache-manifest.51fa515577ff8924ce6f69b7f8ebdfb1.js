c1bd4763554591c572f46a8b4cfa050e72ffae35
adi_peled <adipeled499@gmail.com>
2020-08-27 19:27:53 +0300

    Updates

---
+++ precache-manifest.51fa515577ff8924ce6f69b7f8ebdfb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d32997d05629de3bf4e0d457106e374",
+    "url": "/My-Bitcoin-React/index.html"
+  },
+  {
+    "revision": "c88110d2e085682abf27",
+    "url": "/My-Bitcoin-React/static/css/main.c18371dc.chunk.css"
+  },
+  {
+    "revision": "c59495521f7e517684fc",
+    "url": "/My-Bitcoin-React/static/js/2.2d6de7d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-Bitcoin-React/static/js/2.2d6de7d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c88110d2e085682abf27",
+    "url": "/My-Bitcoin-React/static/js/main.41aa99c6.chunk.js"
+  },
+  {
+    "revision": "d65983941f8594c96f64",
+    "url": "/My-Bitcoin-React/static/js/runtime-main.7efafab2.js"
+  },
+  {
+    "revision": "4c0d88010ad45b33fc22256c10c727bf",
+    "url": "/My-Bitcoin-React/static/media/bitcoin.4c0d8801.png"
+  },
+  {
+    "revision": "bc18f1967391bb878549cc4eed0f65d5",
+    "url": "/My-Bitcoin-React/static/media/bitcoin.bc18f196.svg"
+  }
+]);
